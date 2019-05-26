2e4be5cd7d103b97df025f5f8a71d8e8242eeaaa
Connor Olding <cloningdonor@gmail.com>
2019-05-26 21:05:02 +0200

    add support for getting time on Windows
    also scraps support for absolutely ancient systems

--- src/cpu_time.c
+++ src/cpu_time.c
@@ -2,53 +2,11 @@
 #include "port.h"
 #include "utility.h"
 
-#ifdef IBM_WATC		/* IBM Waterloo-C compiler (same as bsd 4.2) */
-#ifndef BSD
-#define BSD
-#endif
-#ifndef void
-#define void int
-#endif
-#endif
-
-#ifdef ultrix
-#ifndef BSD
-#define BSD
-#endif
-#endif
-
-#ifdef aiws
-#ifndef UNIX10
-#define UNIX10
-#endif
-#endif
-
-#ifdef vms		/* VAX/C compiler -- times() with 100 HZ clock */
-#ifndef UNIX100
-#define UNIX100
-#endif
-#endif
-
-/* default */
-#if !defined(BSD) && !defined(UNIX10) && !defined(UNIX60) && !defined(UNIX100)
-#define BSD
-#endif
-
-#ifdef BSD
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <sys/time.h>
 #include <sys/resource.h>
-#endif
-
-#ifdef UNIX10
-#include <sys/times.h>
-#endif
-
-#ifdef UNIX60
-#include <sys/times.h>
-#endif
-
-#ifdef UNIX100
-#include <sys/times.h>
 #endif
 
 
@@ -60,28 +18,15 @@ long util_cpu_time(void)
 {
     long t = 0;
 
-#ifdef BSD
+#ifdef _WIN32
+    LARGE_INTEGER time, freq;
+    QueryPerformanceCounter(&time);
+    QueryPerformanceFrequency(&freq);
+    t = (long) (time.QuadPart * 1000 / freq.QuadPart);
+#else
     struct rusage rusage;
     (void) getrusage(RUSAGE_SELF, &rusage);
     t = (long) rusage.ru_utime.tv_sec*1000 + rusage.ru_utime.tv_usec/1000;
-#endif
-
-#ifdef UNIX10			/* times() with 10 Hz resolution */
-    struct tms buffer;
-    (void) times(&buffer);
-    t = buffer.tms_utime * 100;
-#endif
-
-#ifdef UNIX60			/* times() with 60 Hz resolution */
-    struct tms buffer;
-    times(&buffer);
-    t = buffer.tms_utime * 16.6667;
-#endif
-
-#ifdef UNIX100
-    struct tms buffer;		/* times() with 100 Hz resolution */
-    times(&buffer);
-    t = buffer.tms_utime * 10;
 #endif
 
     return t;
